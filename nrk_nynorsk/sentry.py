9301e0cbd7b6852e014a8f1a389b9da2d8f1e9c4
henribru <6639509+henribru@users.noreply.github.com>
2020-08-01 16:44:25 +0200

    Add Sentry

---
+++ nrk_nynorsk/sentry.py
@@ -0,0 +1,14 @@
+import os
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+
+def init_sentry():
+    if "SENTRY_DSN" not in os.environ:
+        return
+
+    sentry_sdk.init(
+        dsn=os.environ["SENTRY_DSN"],
+        integrations=[DjangoIntegration()],
+    )
--- nrk_nynorsk/settings.py
+++ nrk_nynorsk/settings.py
@@ -15,7 +15,11 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
+from nrk_nynorsk.sentry import init_sentry
+
 load_dotenv()
+
+init_sentry()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
