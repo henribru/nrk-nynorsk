0e8c71ab7f1c8584767ac82d6d6a0d57da14f85f
henribru <6639509+henribru@users.noreply.github.com>
2020-08-04 14:17:36 +0200

    Improve model admin

--- nrk_nynorsk/admin.py
+++ nrk_nynorsk/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 
-from nrk_nynorsk.models import Article
+from nrk_nynorsk.models import Article, RSSFeed
 
-admin.register(Article)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title", "publication_date"]
+    date_hierarchy = "publication_date"
+
+
+@admin.register(RSSFeed)
+class RSSFeedAdmin(admin.ModelAdmin):
+    list_display = ["url", "last_checked"]
+    date_hierarchy = "last_checked"
