864b65977a8d895e49e21b490b55178c10bcea13
henribru <6639509+henribru@users.noreply.github.com>
2020-12-19 16:36:43 +0100

    Simplify language code

--- nrk_nynorsk/settings.py
+++ nrk_nynorsk/settings.py
@@ -124,7 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = "nn-NO"
+LANGUAGE_CODE = "nn"
 
 TIME_ZONE = "Europe/Oslo"
 
